4dc51733e81d12b708d11cda5bf2d381e1e552ac
Nitin Bhandari <nitin211206@gmail.com>
2025-07-19 15:54:25 +0530

    feat: Added Filter Button

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -1,18 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const init = {
-    search: "",
-}
+  search: "",
+  category: "All",
+  level: "All",
+  sort: "Default",
+};
 
 const searchSlice = createSlice({
-    name: "search",
-    initialState: init,
-    reducers:{
-        setSearch(state,action){
-            state.search = action.payload;
-        }
-    }
-})
+  name: "search",
+  initialState: init,
+  reducers: {
+    setSearch(state, action) {
+      state.search = action.payload;
+    },
+    setCategory(state, action) {
+      state.category = action.payload;
+    },
+    setLevel(state, action) {
+      state.level = action.payload;
+    },
+    setSort(state, action) {
+      state.sort = action.payload;
+    },
+  },
+});
 
-export const {setSearch} = searchSlice.actions;
+export const { setSearch, setCategory, setLevel, setSort } =
+  searchSlice.actions;
 export default searchSlice.reducer;
